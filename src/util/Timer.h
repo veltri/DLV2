444ff4af959b95c9ba0ed090633f3dbb0ef6ea97
dave90 <davide@davide-All-Series>
2015-02-06 13:29:15 +0100

    Finish Timer

--- src/util/Timer.h
+++ src/util/Timer.h
@@ -11,6 +11,7 @@
 #include <unordered_map>
 #include <string>
 #include <time.h>
+#include <iostream>
 
 namespace DLV2 {
 namespace grounder {
@@ -19,15 +20,57 @@ using namespace std;
 
 class Timer {
 public:
-	Timer(){};
 	virtual ~Timer(){};
 
-	virtual void start(string label){
-		auto it=map_time.find(label);
-		if(it==map_time.)
+	void start(string label){
+		clock_t clock_start=clock();
+		auto it=map_clock.find(label);
+		if(it==map_clock.end())
+			map_clock.insert({label,clock_start});
+		else{
+			map_clock.erase(label);
+			map_clock.insert({label,clock_start});
+		}
+
 	}
+
+	void stop(string label){
+		clock_t clock_end=clock();
+		clock_t clock_start=map_clock[label];
+		double time_elapsed=( clock_end - clock_start ) / (double) CLOCKS_PER_SEC;
+
+		auto it=map_elapse_time.find(label);
+		if(it==map_elapse_time.end())
+			map_elapse_time.insert({label,time_elapsed});
+		else{
+			double previus_time=map_elapse_time[label];
+			map_elapse_time.erase(label);
+			map_elapse_time.insert({label,previus_time+time_elapsed});
+		}
+	}
+
+	void print(){
+		cout<<"TIME:"<<endl;
+		for(auto& time_pair:map_elapse_time)
+			cout<<time_pair.first<<" "<<time_pair.second<<endl;
+	}
+
+	static Timer* getInstance(){
+		if(timer_instance==nullptr)
+			timer_instance=new Timer;
+		return timer_instance;
+	}
+
 private:
-	unordered_map<string,clock_t> map_time;
+	Timer(){};
+
+	static Timer* timer_instance;
+
+
+
+	unordered_map<string,clock_t> map_clock;
+	unordered_map<string,double> map_elapse_time;
+
 };
 
 } /* namespace grounder */
